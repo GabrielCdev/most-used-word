c3405eab1f2d05cd465f452b61a2587bfb844d59
GabrielCdev <gabi_borges01@hotmail.com>
2023-03-15 14:58:23 -0300

    Finished!

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,33 +1,20 @@
 <template>
   <v-container fluid>
     <v-form>
-      <v-file-input
-        label="Selecione as legendas"
-        prepend-icon="mdi-message-text"
-        append-outer-icon="mdi-send"
-        outlined
-        multiple
-        chips
-        v-model="files"
-        @click:append-outer="processSubtitles"
-      />
+      <v-file-input label="Selecione as legendas" prepend-icon="mdi-message-text" append-outer-icon="mdi-send" outlined
+        multiple chips v-model="files" @click:append-outer="processSubtitles" />
     </v-form>
 
     <div class="pills">
       <!-- : servem para interpretar o que está sendo chamado. O uso sem : implica em repetir o texto que foi previamente escrito -->
-      <Pill
-        v-for="word in groupedWords"
-        :key="word.name"
-        :name="word.name"
-        :amount="word.amount"
-      />
+      <Pill v-for="word in groupedWords" :key="word.name" :name="word.name" :amount="word.amount" />
     </div>
   </v-container>
 </template>
 
 <script>
 // ipcRenderer = Usado para disparar o evento na interface gráfica quando o usuário selecionar o ProcessSubtitle
-// import { ipcRenderer } from "electron";
+import { ipcRenderer } from "electron";
 import Pill from "./Pill";
 
 export default {
@@ -46,9 +33,9 @@ export default {
 
       // Comunicação async
       // Enviar para o canal de comunicação (process-subtitles) um determinado parâmetro (files)
-      window.Electron.ipcRenderer.send("process-subtitles", paths);
+      ipcRenderer.send("process-subtitles", paths);
       // Receber uma resposta no mesmo canal que envia (process-subtitles) - podia ser em outro canal, basta mudar aqui e no backend)
-      window.Electron.ipcRenderer.on("process-subtitles", (event, resp) => {
+      ipcRenderer.on("process-subtitles", (event, resp) => {
         this.groupedWords = resp;
       });
     },
