9ad11874a90dc0aeeb5ee7b9ef1e31a9faf2e1ab
GabrielCdev <gabi_borges01@hotmail.com>
2023-03-15 11:24:35 -0300

    Created the graphical interface

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container fluid>
+    <v-form>
+      <v-file-input
+        label="Selecione as legendas"
+        prepend-icon="mdi-message-text"
+        append-outer-icon="mdi-send"
+        outlined
+        multiple
+        chips
+        v-model="files"
+        @click:append-outer="processSubtitles"
+      />
+    </v-form>
+
+    <div class="pills">
+      <!-- : servem para interpretar o que está sendo chamado. O uso sem : implica em repetir o texto que foi previamente escrito -->
+      <Pill
+        v-for="word in groupedWords"
+        :key="word.name"
+        :name="word.name"
+        :amount="word.amount"
+      />
+    </div>
+  </v-container>
+</template>
+
+<script>
+import Pill from "./Pill";
+
+export default {
+  components: { Pill },
+
+  data: function () {
+    return {
+      files: [],
+      groupedWords: [
+        { name: "i", amount: 1234 },
+        { name: "you", amount: 900 },
+        { name: "he", amount: 853 },
+      ],
+    };
+  },
+  methods: {
+    processSubtitles() {
+      console.log(this.files);
+    },
+  },
+};
+</script>
+
+<style>
+.pills {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+</style>
